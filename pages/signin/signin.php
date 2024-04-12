4898734786d8a486d1b15303edddfc93bf4cb0f6
dukelw <thevi16102004@gmail.com>
2024-04-12 22:54:44 +0700

    whybug

--- pages/signin/signin.php
+++ pages/signin/signin.php
@@ -1,5 +1,14 @@
-<!doctype html>
-<html lang="en">
+<?php
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+    $success = Teacher::checkLogin($_POST["email"], $_POST["password"]);
+    if ($success) {
+        echo "Đăng nhập thành công";
+        header("Location: ../pages/index.php");
+    } else {
+        echo "Đăng nhập thất bại";
+    }
+}
+?>
 
 <head>
     <meta charset="utf-8">
@@ -12,22 +21,15 @@
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
 
-    <!-- Bootstrap core CSS -->
     <link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
     <link href="./signin.css" rel="stylesheet">
-</head>
-
-<body class="text-center">
-    <form class="form-signin">
-        <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72"
-            height="72">
+    <form class="form-signin" action="#" method="POST">
+        <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <input name="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <div class="checkbox mb-3">
             <label>
                 <input type="checkbox" value="remember-me"> Remember me
@@ -37,6 +39,6 @@
         <p class="mt-4">Chưa có tài khoản liên hệ <a href="#">thầy Dzoãn Thanh đẹp trai</a> để lấy tài khoản</p>
         <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
     </form>
-</body>
+    </body>
 
-</html>
+    </html>

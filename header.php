7555e351babc852732ad0ce7c9cd83338f020c86
dukelw <thevi16102004@gmail.com>
2024-04-19 23:54:58 +0700

    complete search

--- header.php
+++ header.php
@@ -1,22 +1,66 @@
+<?php
+include_once("./entities/article.class.php");
+session_start();
+?>
+
 <link rel="stylesheet" href="./css/header.css">
 <header class="blog-header py-3">
   <div class="row flex-nowrap justify-content-between align-items-center">
-    <div class="col-4 pt-1">
-      <a class="text-muted" href="#">Về trang chủ</a>
+    <div class="col-4 pt-1 d-flex justify-content-center align-items-start flex-column search-container">
+      <div class="col-12 pt-1 d-flex justify-content-start align-items-center">
+        <span class="text-muted search-toggle">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3">
+            <circle cx="10.5" cy="10.5" r="7.5"></circle>
+            <line x1="21" y1="21" x2="15.8" y2="15.8"></line>
+          </svg>
+        </span>
+        <form action="#" class="search-wrapper">
+          <input class="search-box" type="text" placeholder="Nhập từ khóa...">
+          <span class="search-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3">
+              <circle cx="10.5" cy="10.5" r="7.5"></circle>
+              <line x1="21" y1="21" x2="15.8" y2="15.8"></line>
+            </svg>
+          </span>
+          <input type="text" hidden name="keysearch">
+        </form>
+      </div>
+      <ul class='search-results'>
+        <?php
+        $articlesPerPage = 6;
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+        $start = ($page - 1) * $articlesPerPage;
+        if (isset($_GET['keysearch']) && $_GET['keysearch'] != '') {
+          $keySearch = $_GET['keysearch'];
+          $foundArticles = Article::list_articles_by_keyword($keySearch, $start, $articlesPerPage);
+          echo "<li class='text-right close-btn'> &times </li>";
+          if (count($foundArticles) != 0) {
+            foreach ($foundArticles as $foundArticle) {
+              echo
+              "<li class=''>
+                  <div class='d-flex justify-content-between align-items-center'>
+                    <img class='news-image' src='" . $foundArticle["THUMBNAIL"] . "' alt='Thumbnail'/>
+                    <p>
+                      <a href='article_detail.php?id=" . $foundArticle["ID"] . "' class='notification-title'> " . $foundArticle["TITLE"] . "</a>
+                      <span class='publish-day'>" . $foundArticle["PUBLISH"] . "</span>
+                    </p>
+                  </div>
+                </li>";
+            }
+          } else {
+            echo "<li>Không có bài báo nào được tìm thấy</li>";
+          }
+        }
+        ?>
+      </ul>
     </div>
     <div class="col-4 text-center">
       <a class="blog-header-logo text-dark" href="main.php">Khoa công nghệ thông tin</a>
     </div>
     <div class="col-4 d-flex justify-content-end align-items-center">
-      <a class="text-muted" href="#">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3">
-          <circle cx="10.5" cy="10.5" r="7.5"></circle>
-          <line x1="21" y1="21" x2="15.8" y2="15.8"></line>
-        </svg>
-      </a>
       <?php
       include_once("./entities/teacher.class.php");
-      session_start();
       if (isset($_SESSION['username']) && $_SESSION['username'] != "") {
         echo "
           <div class='user-info'>
@@ -51,12 +95,49 @@
 
 <script>
   const user = document.querySelector(".user-info")
-  user.onclick = () => {
+
+  user.onmouseenter = () => {
     const optionsForm = document.querySelector('.user-options')
-    if (optionsForm.classList.contains('active')) {
-      optionsForm.classList.remove('active')
-    } else {
-      optionsForm.classList.add('active')
+    optionsForm.classList.add('active')
+  }
+
+  user.onmouseleave = () => {
+    const optionsForm = document.querySelector('.user-options')
+    optionsForm.classList.remove('active')
+  }
+
+  const searchToggle = document.querySelector('.search-toggle')
+  const searchWrapper = document.querySelector('.search-wrapper')
+  const searchBox = document.querySelector('.search-box')
+  const searchIcon = document.querySelector('.search-icon')
+  const hiddenInput = document.querySelector('input[name="keysearch"]')
+  const closeBtn = document.querySelector('.close-btn')
+  searchToggle.onclick = function(event) {
+    searchWrapper.classList.add('active-flex');
+    searchToggle.classList.add('hide')
+    searchBox.focus()
+  }
+
+  searchIcon.onclick = function(event) {
+    var keySearch = searchBox.value.trim()
+    hiddenInput.value = keySearch
+    searchWrapper.submit();
+  }
+
+  searchBox.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      var keySearch = searchBox.value.trim();
+      hiddenInput.value = keySearch;
+      searchWrapper.submit();
+    }
+  });
+
+
+  if (closeBtn != null) {
+    closeBtn.onclick = function(event) {
+      hiddenInput.value = ''
+      searchWrapper.submit();
     }
   }
 </script>

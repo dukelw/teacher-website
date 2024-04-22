a4fe94ddf9c97bff7a3340ba14539e3846594a79
Wzinh <vinhquang2610345@gmail.com>
2024-04-23 00:04:17 +0700

    complete introduction

---
+++ introduction-sidebar.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="./css/introduction-sidebar.css">
+<div class="sidebar-inner">
+    <div>
+        <div class="block">
+            <h2 class="block-title">
+                Giới thiệu
+            </h2>
+            <img src="https://it.tdtu.edu.vn/sites/cntt/files/inline-images/L%E1%BB%8Bch%20s%E1%BB%AD%20h%C3%ACnh%20th%C3%A0nh.jpg" alt="">
+            <p>
+                <a href="./introduction-faculty.php">Giới thiệu Khoa</a>
+            </p>
+            <p>
+                <a href="./introduction-computerscience.php">Bộ môn Khoa học máy tính</a>
+            </p>
+            <p>
+                <a href="./introduction-softwareengineering.php">Bộ môn Công nghệ phần mềm</a>
+            </p>
+            <p>
+                <a href="./introduction-informationsystem.php">Bộ môn Hệ thống thông tin</a>
+            </p>
+            <p>
+                <a href="./introduction-computernetwork.php">Bộ môn Mạng máy tính</a>
+            </p>
+            <p class="back">
+                <a href="./introduction.php">Về Giới thiệu</a>
+            </p>
+        </div>
+    </div>
+</div>

9b4429fce416c2a5121b3e0415153fe9f91720ca
vixtruong <vipro20161@gmail.com>
2024-05-08 00:03:48 +0700

    new

--- update_info.php
+++ update_info.php
@@ -1,7 +1,16 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+  rel="stylesheet"
+  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+  crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+  crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+  integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+  crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+  integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+  crossorigin="anonymous"></script>
 <link href="./assets/css/blog.css" rel="stylesheet">
 <title>Teacher Website</title>
 <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/blog/">
@@ -14,7 +23,7 @@
 <link rel="stylesheet" href="./assets/icon/themify-icons/themify-icons.css">
 <?php
 echo "<div class='container'>";
-include_once("./entities/teacher.class.php");
+include_once ("./entities/user.class.php");
 // include_once("./header.php");
 session_start();
 echo "</div>";
@@ -30,7 +39,7 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
   $month = $_POST["birthdayMonth"];
   $year = $_POST["birthdayYear"];
   $birthday = $year . "-" . $month . "-" . $day;
-  $result = Teacher::update_information($_SESSION["useremail"], $thumbnail, $name, $email, $address, $phone, $gender, $birthday);
+  $result = User::update_information($_SESSION["useremail"], $thumbnail, $name, $email, $address, $phone, $gender, $birthday);
   if ($result) {
     header("Location main.php");
   } else {
@@ -39,17 +48,27 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
 }
 ?>
 
-<form enctype="multipart/form-data" action="update_info.php" method="POST" class="container rounded bg-white mt-5 mb-5">
+<form enctype="multipart/form-data" action="update_info.php" method="POST"
+  class="container rounded bg-white mt-5 mb-5">
   <div class="row">
     <div class="col-md-3 border-right">
-      <div style="padding-bottom: 12px !important;" class="d-flex flex-column align-items-center text-center py-5"><img class="user-avatar-big rounded-circle" src="<?= Teacher::get_teacher($_SESSION['useremail'])[0]["AVATAR"] ?>"> <span class="font-weight-bold"><?= Teacher::get_teacher($_SESSION['useremail'])[0]["NAME"] ?></span><span class="text-black-50"><?= Teacher::get_teacher($_SESSION['useremail'])[0]["MAIL"] ?></span><span> </span></div>
+      <div style="padding-bottom: 12px !important;"
+        class="d-flex flex-column align-items-center text-center py-5"><img
+          class="user-avatar-big rounded-circle"
+          src="<?= User::get_user($_SESSION['useremail'])[0]["AVATAR"] ?>"> <span
+          class="font-weight-bold"><?= User::get_user($_SESSION['useremail'])[0]["NAME"] ?></span><span
+          class="text-black-50"><?= User::get_user($_SESSION['useremail'])[0]["MAIL"] ?></span><span>
+        </span></div>
       <div class="col-md-12">
         <label for="thumbnail" class="form-label">Đổi hình nền</label>
-        <input disabled type="file" accept=".PNG, .GIF, .JPG" name="thumbnail" class="form-control" id="thumbnail">
+        <input disabled type="file" accept=".PNG, .GIF, .JPG" name="thumbnail" class="form-control"
+          id="thumbnail">
       </div>
-      <div style="visibility: hidden;" class="col-md-12 d-flex flex-column align-items-center thumbnail-wrapper">
+      <div style="visibility: hidden;"
+        class="col-md-12 d-flex flex-column align-items-center thumbnail-wrapper">
         <h4 class="mt-4">Ảnh xem trước</h4>
-        <img id="preview" class="user-avatar-big rounded-circle" src="<?= Teacher::get_teacher($_SESSION['useremail'])[0]["AVATAR"] ?>">
+        <img id="preview" class="user-avatar-big rounded-circle"
+          src="<?= User::get_user($_SESSION['useremail'])[0]["AVATAR"] ?>">
       </div>
     </div>
     <div class="col-md-9 border-right">
@@ -59,16 +78,24 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
           <button class="btn btn-primary edit-btn" type="button">Chỉnh sửa</button>
         </div>
         <div class="row mt-3">
-          <div class="col-md-12"><label class="labels">Tên</label><input disabled type="text" name="name" class="form-control" value="<?= Teacher::get_teacher($_SESSION['useremail'])[0]["NAME"] ?>"></div>
-          <div class="col-md-12"><label class="labels">Email</label><input disabled type="text" name="email" class="form-control" value="<?= Teacher::get_teacher($_SESSION['useremail'])[0]["MAIL"] ?>"></div>
-          <div class="col-md-12"><label class="labels">Số điện thoại</label><input disabled type="text" name="phone" class="form-control" value="<?= Teacher::get_teacher($_SESSION['useremail'])[0]["PHONE"] ?>"></div>
-          <div class="col-md-12"><label class="labels">Địa chỉ</label><input disabled type="text" name="address" class="form-control" value="<?= Teacher::get_teacher($_SESSION['useremail'])[0]["ADDRESS"] ?>"></div>
+          <div class="col-md-12"><label class="labels">Tên</label><input disabled type="text"
+              name="name" class="form-control"
+              value="<?= User::get_user($_SESSION['useremail'])[0]["NAME"] ?>"></div>
+          <div class="col-md-12"><label class="labels">Email</label><input disabled type="text"
+              name="email" class="form-control"
+              value="<?= User::get_user($_SESSION['useremail'])[0]["MAIL"] ?>"></div>
+          <div class="col-md-12"><label class="labels">Số điện thoại</label><input disabled
+              type="text" name="phone" class="form-control"
+              value="<?= User::get_user($_SESSION['useremail'])[0]["PHONE"] ?>"></div>
+          <div class="col-md-12"><label class="labels">Địa chỉ</label><input disabled type="text"
+              name="address" class="form-control"
+              value="<?= User::get_user($_SESSION['useremail'])[0]["ADDRESS"] ?>"></div>
           <div class="col-md-12">
             <label class="labels">Giới tính</label>
             <select name="gender" disabled type="text" class="form-control">
               <option value="_">Giới tính</option>
-              <option value="1" <?php echo (Teacher::get_teacher($_SESSION['useremail'])[0]["GENDER"] == 1) ? 'selected' : ''; ?>>Nam</option>
-              <option value="0" <?php echo (Teacher::get_teacher($_SESSION['useremail'])[0]["GENDER"] == 0) ? 'selected' : ''; ?>>Nữ</option>
+              <option value="1" <?php echo (User::get_user($_SESSION['useremail'])[0]["GENDER"] == 1) ? 'selected' : ''; ?>>Nam</option>
+              <option value="0" <?php echo (User::get_user($_SESSION['useremail'])[0]["GENDER"] == 0) ? 'selected' : ''; ?>>Nữ</option>
             </select>
           </div>
         </div>
@@ -79,10 +106,11 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
               <div class="col">
                 <select disabled name="birthdayDay" class="form-control">
                   <?php
-                  $selectedDay = date('d', strtotime(Teacher::get_teacher($_SESSION['useremail'])[0]["BIRTHDAY"]));
+                  $selectedDay = date('d', strtotime(User::get_user($_SESSION['useremail'])[0]["BIRTHDAY"]));
                   for ($day = 1; $day <= 31; $day++) {
                     echo '<option value="' . $day . '"';
-                    if ($day == $selectedDay) echo ' selected';
+                    if ($day == $selectedDay)
+                      echo ' selected';
                     echo '>' . $day . '</option>';
                   }
                   ?>
@@ -91,7 +119,7 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
               <div class="col">
                 <select disabled name="birthdayMonth" class="form-control">
                   <?php
-                  $selectedMonth = date('m', strtotime(Teacher::get_teacher($_SESSION['useremail'])[0]["BIRTHDAY"]));
+                  $selectedMonth = date('m', strtotime(User::get_user($_SESSION['useremail'])[0]["BIRTHDAY"]));
                   $months = [
                     '01' => 'Tháng 1',
                     '02' => 'Tháng 2',
@@ -108,7 +136,8 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
                   ];
                   foreach ($months as $key => $month) {
                     echo '<option value="' . $key . '"';
-                    if ($key == $selectedMonth) echo ' selected';
+                    if ($key == $selectedMonth)
+                      echo ' selected';
                     echo '>' . $month . '</option>';
                   }
                   ?>
@@ -120,10 +149,11 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
                   $currentYear = date('Y');
                   $startYear = $currentYear - 100;
                   $endYear = $currentYear;
-                  $selectedYear = date('Y', strtotime(Teacher::get_teacher($_SESSION['useremail'])[0]["BIRTHDAY"]));
+                  $selectedYear = date('Y', strtotime(User::get_user($_SESSION['useremail'])[0]["BIRTHDAY"]));
                   for ($year = $endYear; $year >= $startYear; $year--) {
                     echo '<option value="' . $year . '"';
-                    if ($year == $selectedYear) echo ' selected';
+                    if ($year == $selectedYear)
+                      echo ' selected';
                     echo '>' . $year . '</option>';
                   }
                   ?>
@@ -137,10 +167,11 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
               <div class="col">
                 <select disabled name="joinDay" class="form-control">
                   <?php
-                  $selectedDay = date('d', strtotime(Teacher::get_teacher($_SESSION['useremail'])[0]["JOINYEAR"]));
+                  $selectedDay = date('d', strtotime(User::get_user($_SESSION['useremail'])[0]["JOINYEAR"]));
                   for ($day = 1; $day <= 31; $day++) {
                     echo '<option value="' . $day . '"';
-                    if ($day == $selectedDay) echo ' selected';
+                    if ($day == $selectedDay)
+                      echo ' selected';
                     echo '>' . $day . '</option>';
                   }
                   ?>
@@ -149,7 +180,7 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
               <div class="col">
                 <select disabled name="joinMonth" class="form-control">
                   <?php
-                  $selectedMonth = date('m', strtotime(Teacher::get_teacher($_SESSION['useremail'])[0]["JOINYEAR"]));
+                  $selectedMonth = date('m', strtotime(User::get_user($_SESSION['useremail'])[0]["JOINYEAR"]));
                   $months = [
                     '01' => 'Tháng 1',
                     '02' => 'Tháng 2',
@@ -166,7 +197,8 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
                   ];
                   foreach ($months as $key => $month) {
                     echo '<option value="' . $key . '"';
-                    if ($key == $selectedMonth) echo ' selected';
+                    if ($key == $selectedMonth)
+                      echo ' selected';
                     echo '>' . $month . '</option>';
                   }
                   ?>
@@ -178,10 +210,11 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
                   $currentYear = date('Y');
                   $startYear = $currentYear - 100;
                   $endYear = $currentYear;
-                  $selectedYear = date('Y', strtotime(Teacher::get_teacher($_SESSION['useremail'])[0]["JOINYEAR"]));
+                  $selectedYear = date('Y', strtotime(User::get_user($_SESSION['useremail'])[0]["JOINYEAR"]));
                   for ($year = $endYear; $year >= $startYear; $year--) {
                     echo '<option value="' . $year . '"';
-                    if ($year == $selectedYear) echo ' selected';
+                    if ($year == $selectedYear)
+                      echo ' selected';
                     echo '>' . $year . '</option>';
                   }
                   ?>
@@ -190,13 +223,14 @@ if (isset($_POST["name"]) || isset($_POST["btnsubmit"]) || isset($_FILES["thumbn
             </div>
           </div>
         </div>
-        <div class="mt-5 text-center"><button class="btn btn-primary profile-button save-btn" name="btnsubmit" type="submit" disabled>Lưu thay đổi</button></div>
+        <div class="mt-5 text-center"><button class="btn btn-primary profile-button save-btn"
+            name="btnsubmit" type="submit" disabled>Lưu thay đổi</button></div>
       </div>
     </div>
   </div>
 </form>
 <?php
-include_once("footer.php");
+include_once ("footer.php");
 ?>
 <script>
   const editBtn = document.querySelector('.edit-btn');
@@ -210,7 +244,7 @@ include_once("footer.php");
   const birthdayDay = document.querySelector('select[name="birthdayDay"]');
   const birthdayMonth = document.querySelector('select[name="birthdayMonth"]');
   const birthdayYear = document.querySelector('select[name="birthdayYear"]');
-  editBtn.onclick = function() {
+  editBtn.onclick = function () {
     const isDisabled = name.disabled;
     saveBtn.disabled = !isDisabled;
     name.disabled = !isDisabled;
@@ -239,10 +273,10 @@ include_once("footer.php");
   const thumbnailInput = document.querySelector('#thumbnail');
   const previewImg = document.querySelector('#preview');
 
-  thumbnailInput.addEventListener('change', function() {
+  thumbnailInput.addEventListener('change', function () {
     if (thumbnailInput.files && thumbnailInput.files[0]) {
       const reader = new FileReader();
-      reader.onload = function(e) {
+      reader.onload = function (e) {
         previewImg.src = e.target.result;
       };
 
